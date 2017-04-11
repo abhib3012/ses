166c98ff7c52b8dfd0408967903a7f4f46db736e
Abhishek Bhattacharya <dhaval@dr4cun0.com>
2017-04-11 11:52:38 +0000

    Tidy Code

--- core/dataSave.js
+++ core/dataSave.js
@@ -1,69 +1,61 @@
 var reontology = require('re-ontology');
 //console.log(reontology)
-
 var people = new reontology.Type({
-    name: "Person",
-    features: {
-        Type: "Human"
-    }
+	name: "Person",
+	features: {
+		Type: "Human"
+	}
 })
-
 var men = people.addSubType({
-    name: 'Male',
-    features: {
-        Gender: 'Male'
-    }
+	name: 'Male',
+	features: {
+		Gender: 'Male'
+	}
 })
 var women = people.addSubType({
-    name: 'Female',
-    features: {
-        Gender: 'Female'
-    }
+	name: 'Female',
+	features: {
+		Gender: 'Female'
+	}
 })
-
 men.addInstance({
-    name: 'Robert',
-    description: {
-        father: 'James',
-        mother: 'Gladice',
-        sons: ['Eric'],
-        daughters: ['Susan', 'Carol', 'Annabelle']
-    }
+	name: 'Robert',
+	description: {
+		father: 'James',
+		mother: 'Gladice',
+		sons: ['Eric'],
+		daughters: ['Susan', 'Carol', 'Annabelle']
+	}
 })
- 
 men.addInstance({
-    name: 'Eric',
-    description: {
-        father: 'Robert',
-        mother: 'Sylvia',
-        sons: ['James', 'Wendell'],
-        daughters: ['Emily']
-    }
+	name: 'Eric',
+	description: {
+		father: 'Robert',
+		mother: 'Sylvia',
+		sons: ['James', 'Wendell'],
+		daughters: ['Emily']
+	}
 })
- 
 women.addInstance({
-    name: 'Susan',
-    description: {
-        father: 'Robert',
-        mother: 'Sylvia',
-        sons: null,
-        daughters: null
-    }
+	name: 'Susan',
+	description: {
+		father: 'Robert',
+		mother: 'Sylvia',
+		sons: null,
+		daughters: null
+	}
 })
- 
 women.addInstance({
-    name: 'Samanthat',
-    description: {
-        father: 'Adam',
-        mother: 'Beth',
-        sons: null,
-        daughters: null
-    }
+	name: 'Samanthat',
+	description: {
+		father: 'Adam',
+		mother: 'Beth',
+		sons: null,
+		daughters: null
+	}
 })
-
-people.createTable('people').then(function(){
-    people.save();
+people.createTable('people').then(function() {
+	people.save();
 });
-
 console.log(people.viewInstance('Male'))
 console.log(people.viewInstance('Female'))
